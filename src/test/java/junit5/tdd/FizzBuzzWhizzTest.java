7f9a5aea7315b7d9932f08e2083c85b5adaca0b6
ELLIE Y Y SU (ITA-ISDC-ISD-OOCLL/ZHA) <SUEL2@OOCL.COM>
2020-07-23 13:16:36 +0800

    [FizzBuzzWhizz] test: should_return_Fizz_when_count_of_given_3

--- src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
@@ -7,15 +7,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzWhizzTest {
 
     @Test
-     void should_return_1_when_count_of_given_1(){
-         //given
-         FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
-         int number = 1;
+    void should_return_1_when_count_of_given_1() {
+        //given
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        int number = 1;
 
-         //when
-         String result = fizzBuzzWhizz.countOf(number);
+        //when
+        String result = fizzBuzzWhizz.countOf(number);
 
-         //then
-         assertEquals("1", result);
-     }
+        //then
+        assertEquals("1", result);
+    }
+
+    @Test
+    void should_return_Fizz_when_count_of_given_3() {
+        //given
+        int number = 3;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        //when
+        String result = fizzBuzzWhizz.countOf(number);
+
+        //then
+        assertEquals("Fizz", result);
+    }
 }
