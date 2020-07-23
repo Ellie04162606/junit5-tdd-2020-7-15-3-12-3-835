07050f8f231d7c8af40ad3017af56495a4968368
ELLIE Y Y SU (ITA-ISDC-ISD-OOCLL/ZHA) <SUEL2@OOCL.COM>
2020-07-23 13:18:25 +0800

    [FizzBuzzWhizz] pass should_return_Fizz_when_count_of_given_3

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -2,6 +2,9 @@ package junit5.tdd;
 
 public class FizzBuzzWhizz {
     public String countOf(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
